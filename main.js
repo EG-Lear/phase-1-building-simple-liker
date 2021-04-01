363a064523f8c273754a1a9129328f7b696792ca
Garret <gwiats@gmail.com>
2021-03-31 22:12:21 -0400

    Done.

--- main.js
+++ main.js
@@ -3,9 +3,38 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.like').forEach(like => {
+    like.addEventListener('click', heartMod)
+  }) 
+})
 
+document.getElementById('modal').classList.add('hidden') // hides error
 
+function heartMod(event) { //makes hearts red
+  const target = event.target.querySelector('span')
+  mimicServerCall()
+  .then(() => {
+    if (target.textContent === `${FULL_HEART}`) {
+      console.log('testing remove')
+      target.classList.remove('activated-heart')
+      target.textContent = EMPTY_HEART
+    } else {
+      console.log('testing add')
+      target.classList.add('activated-heart')
+      target.textContent = FULL_HEART
+    }
+  }) 
+  .catch(res => {
+    setTimeout(addHidden, 3000)
+    document.getElementById('modal').classList.remove('hidden')
+    document.getElementById('modal-message').innerHTML = res
+  })
+}
 
+function addHidden() {
+  document.getElementById('modal').classList.add('hidden')
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
